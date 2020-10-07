8698db8dfb9316e2b6e914c66e734a61547ce9cb
TheTrufth <zcabda1@ucl.ac.uk>
2020-10-07 16:17:19 +0100

    addin my name

--- assignments/assignment1/test.py
+++ assignments/assignment1/test.py
@@ -1,16 +1,16 @@
-<<<<<<< HEAD
-def gcd(a, b):	while not a == b:		if a > b:			a = a - b		else:			b = b - a	return aax = 42bx = 30print("GCD of", ax, "and", bx, "is", gcd(ax, bx))print("I just modified this now at Wed 7th October")print("Another one")
-=======
-def gcd(a, b):
-    while not a == b:
-        if a > b:
-            a = a - b
-        else:
-            b = b - a
-    return a
 
+def gcd(a, b):
+	while not a == b:
+		if a > b:
+			a = a - b
+		else:
+			b = b - a
+	return a
 ax = 42
 bx = 30
 print("GCD of", ax, "and", bx, "is", gcd(ax, bx))
+print("Dinesh anantharaja wrote this message")
 
->>>>>>> 4eda71ffbd6447e98a207b4ad10fd08b9e9d494c
+
+
+
