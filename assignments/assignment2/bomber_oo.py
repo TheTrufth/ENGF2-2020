2ff6f3ac904d23b240a991c225c5e85a72f3e7f6
TheTrufth <zcabda1@ucl.ac.uk>
2020-10-19 18:28:08 +0100

    Final project

--- assignments/assignment2/bomber_oo.py
+++ assignments/assignment2/bomber_oo.py
@@ -3,13 +3,14 @@ from tkinter import font
 from math import sqrt
 from random import *
 from time import time
+import random
 
 #some global constants
-CANVAS_WIDTH = 1000
+CANVAS_WIDTH = 1200
 CANVAS_HEIGHT = 700
 SPACING = 100
 
-speed = 0.0
+speed = 1.0
 
 class Point(object):
     '''Creates a point on a coordinate plane with values x and y.'''
@@ -43,7 +44,7 @@ class Point(object):
     def distance(self, other):
         dx = self.X - other.X
         dy = self.Y - other.Y
-        return math.sqrt(dx**2 + dy**2)
+        return sqrt(dx*dx + dy*dy)
 
 ''' update_position takes a list of x and y coordinates and a Point.
     It creates a new list of x and y coordintes by adding the Point to all
@@ -62,12 +63,18 @@ def update_position(position_list, position):
 
 ''' The Building class holds all the state associated with one building '''
 class Building():
-    def __init__(self, canvas, building_num, height, width):
+    
+    
+    def __init__(self, canvas, building_num, height, width,):
         self.canvas = canvas
         self.height = height
         self.x = building_num*SPACING
         self.width = width
+
+        if self.x >= CANVAS_WIDTH:
+            return
         self.main_rect = canvas.create_rectangle(self.x, CANVAS_HEIGHT, self.x + self.width, CANVAS_HEIGHT-self.height, fill="brown")
+        
 
     ''' is_inside tests if a point is inside the building '''
     def is_inside(self, point):
@@ -77,10 +84,90 @@ class Building():
 
     ''' shrink the building when a bomb drops on it '''
     def shrink(self):
+        if self.height <= 0:
+            self.cleanup()
+            print("Building destroyed")
         self.height = self.height - 50
         self.canvas.delete(self.main_rect)
-        self.main_rect = self.canvas.create_rectangle(self.x, CANVAS_HEIGHT, self.x + self.width, CANVAS_HEIGHT-self.height, fill="brown")
-
+        COLORS = ['snow', 'ghost white', 'white smoke', 'gainsboro', 'floral white', 'old lace',
+        'linen', 'antique white', 'papaya whip', 'blanched almond', 'bisque', 'peach puff',
+        'navajo white', 'lemon chiffon', 'mint cream', 'azure', 'alice blue', 'lavender',
+        'lavender blush', 'misty rose', 'dark slate gray', 'dim gray', 'slate gray',
+        'light slate gray', 'gray', 'light grey', 'midnight blue', 'navy', 'cornflower blue', 'dark slate blue',
+        'slate blue', 'medium slate blue', 'light slate blue', 'medium blue', 'royal blue',  'blue',
+        'dodger blue', 'deep sky blue', 'sky blue', 'light sky blue', 'steel blue', 'light steel blue',
+        'light blue', 'powder blue', 'pale turquoise', 'dark turquoise', 'medium turquoise', 'turquoise',
+        'cyan', 'light cyan', 'cadet blue', 'medium aquamarine', 'aquamarine', 'dark green', 'dark olive green',
+        'dark sea green', 'sea green', 'medium sea green', 'light sea green', 'pale green', 'spring green',
+        'lawn green', 'medium spring green', 'green yellow', 'lime green', 'yellow green',
+        'forest green', 'olive drab', 'dark khaki', 'khaki', 'pale goldenrod', 'light goldenrod yellow',
+        'light yellow', 'yellow', 'gold', 'light goldenrod', 'goldenrod', 'dark goldenrod', 'rosy brown',
+        'indian red', 'saddle brown', 'sandy brown',
+        'dark salmon', 'salmon', 'light salmon', 'orange', 'dark orange',
+        'coral', 'light coral', 'tomato', 'orange red', 'red', 'hot pink', 'deep pink', 'pink', 'light pink',
+        'pale violet red', 'maroon', 'medium violet red', 'violet red',
+        'medium orchid', 'dark orchid', 'dark violet', 'blue violet', 'purple', 'medium purple',
+        'thistle', 'snow2', 'snow3',
+        'snow4', 'seashell2', 'seashell3', 'seashell4', 'AntiqueWhite1', 'AntiqueWhite2',
+        'AntiqueWhite3', 'AntiqueWhite4', 'bisque2', 'bisque3', 'bisque4', 'PeachPuff2',
+        'PeachPuff3', 'PeachPuff4', 'NavajoWhite2', 'NavajoWhite3', 'NavajoWhite4',
+        'LemonChiffon2', 'LemonChiffon3', 'LemonChiffon4', 'cornsilk2', 'cornsilk3',
+        'cornsilk4', 'ivory2', 'ivory3', 'ivory4', 'honeydew2', 'honeydew3', 'honeydew4',
+        'LavenderBlush2', 'LavenderBlush3', 'LavenderBlush4', 'MistyRose2', 'MistyRose3',
+        'MistyRose4', 'azure2', 'azure3', 'azure4', 'SlateBlue1', 'SlateBlue2', 'SlateBlue3',
+        'SlateBlue4', 'RoyalBlue1', 'RoyalBlue2', 'RoyalBlue3', 'RoyalBlue4', 'blue2', 'blue4',
+        'DodgerBlue2', 'DodgerBlue3', 'DodgerBlue4', 'SteelBlue1', 'SteelBlue2',
+        'SteelBlue3', 'SteelBlue4', 'DeepSkyBlue2', 'DeepSkyBlue3', 'DeepSkyBlue4',
+        'SkyBlue1', 'SkyBlue2', 'SkyBlue3', 'SkyBlue4', 'LightSkyBlue1', 'LightSkyBlue2',
+        'LightSkyBlue3', 'LightSkyBlue4', 'SlateGray1', 'SlateGray2', 'SlateGray3',
+        'SlateGray4', 'LightSteelBlue1', 'LightSteelBlue2', 'LightSteelBlue3',
+        'LightSteelBlue4', 'LightBlue1', 'LightBlue2', 'LightBlue3', 'LightBlue4',
+        'LightCyan2', 'LightCyan3', 'LightCyan4', 'PaleTurquoise1', 'PaleTurquoise2',
+        'PaleTurquoise3', 'PaleTurquoise4', 'CadetBlue1', 'CadetBlue2', 'CadetBlue3',
+        'CadetBlue4', 'turquoise1', 'turquoise2', 'turquoise3', 'turquoise4', 'cyan2', 'cyan3',
+        'cyan4', 'DarkSlateGray1', 'DarkSlateGray2', 'DarkSlateGray3', 'DarkSlateGray4',
+        'aquamarine2', 'aquamarine4', 'DarkSeaGreen1', 'DarkSeaGreen2', 'DarkSeaGreen3',
+        'DarkSeaGreen4', 'SeaGreen1', 'SeaGreen2', 'SeaGreen3', 'PaleGreen1', 'PaleGreen2',
+        'PaleGreen3', 'PaleGreen4', 'SpringGreen2', 'SpringGreen3', 'SpringGreen4',
+        'green2', 'green3', 'green4', 'chartreuse2', 'chartreuse3', 'chartreuse4',
+        'OliveDrab1', 'OliveDrab2', 'OliveDrab4', 'DarkOliveGreen1', 'DarkOliveGreen2',
+        'DarkOliveGreen3', 'DarkOliveGreen4', 'khaki1', 'khaki2', 'khaki3', 'khaki4',
+        'LightGoldenrod1', 'LightGoldenrod2', 'LightGoldenrod3', 'LightGoldenrod4',
+        'LightYellow2', 'LightYellow3', 'LightYellow4', 'yellow2', 'yellow3', 'yellow4',
+        'gold2', 'gold3', 'gold4', 'goldenrod1', 'goldenrod2', 'goldenrod3', 'goldenrod4',
+        'DarkGoldenrod1', 'DarkGoldenrod2', 'DarkGoldenrod3', 'DarkGoldenrod4',
+        'RosyBrown1', 'RosyBrown2', 'RosyBrown3', 'RosyBrown4', 'IndianRed1', 'IndianRed2',
+        'IndianRed3', 'IndianRed4', 'sienna1', 'sienna2', 'sienna3', 'sienna4', 'burlywood1',
+        'burlywood2', 'burlywood3', 'burlywood4', 'wheat1', 'wheat2', 'wheat3', 'wheat4', 'tan1',
+        'tan2', 'tan4', 'chocolate1', 'chocolate2', 'chocolate3', 'firebrick1', 'firebrick2',
+        'firebrick3', 'firebrick4', 'brown1', 'brown2', 'brown3', 'brown4', 'salmon1', 'salmon2',
+        'salmon3', 'salmon4', 'LightSalmon2', 'LightSalmon3', 'LightSalmon4', 'orange2',
+        'orange3', 'orange4', 'DarkOrange1', 'DarkOrange2', 'DarkOrange3', 'DarkOrange4',
+        'coral1', 'coral2', 'coral3', 'coral4', 'tomato2', 'tomato3', 'tomato4', 'OrangeRed2',
+        'OrangeRed3', 'OrangeRed4', 'red2', 'red3', 'red4', 'DeepPink2', 'DeepPink3', 'DeepPink4',
+        'HotPink1', 'HotPink2', 'HotPink3', 'HotPink4', 'pink1', 'pink2', 'pink3', 'pink4',
+        'LightPink1', 'LightPink2', 'LightPink3', 'LightPink4', 'PaleVioletRed1',
+        'PaleVioletRed2', 'PaleVioletRed3', 'PaleVioletRed4', 'maroon1', 'maroon2',
+        'maroon3', 'maroon4', 'VioletRed1', 'VioletRed2', 'VioletRed3', 'VioletRed4',
+        'magenta2', 'magenta3', 'magenta4', 'orchid1', 'orchid2', 'orchid3', 'orchid4', 'plum1',
+        'plum2', 'plum3', 'plum4', 'MediumOrchid1', 'MediumOrchid2', 'MediumOrchid3',
+        'MediumOrchid4', 'DarkOrchid1', 'DarkOrchid2', 'DarkOrchid3', 'DarkOrchid4',
+        'purple1', 'purple2', 'purple3', 'purple4', 'MediumPurple1', 'MediumPurple2',
+        'MediumPurple3', 'MediumPurple4', 'thistle1', 'thistle2', 'thistle3', 'thistle4',
+        'gray1', 'gray2', 'gray3', 'gray4', 'gray5', 'gray6', 'gray7', 'gray8', 'gray9', 'gray10',
+        'gray11', 'gray12', 'gray13', 'gray14', 'gray15', 'gray16', 'gray17', 'gray18', 'gray19',
+        'gray20', 'gray21', 'gray22', 'gray23', 'gray24', 'gray25', 'gray26', 'gray27', 'gray28',
+        'gray29', 'gray30', 'gray31', 'gray32', 'gray33', 'gray34', 'gray35', 'gray36', 'gray37',
+        'gray38', 'gray39', 'gray40', 'gray42', 'gray43', 'gray44', 'gray45', 'gray46', 'gray47',
+        'gray48', 'gray49', 'gray50', 'gray51', 'gray52', 'gray53', 'gray54', 'gray55', 'gray56',
+        'gray57', 'gray58', 'gray59', 'gray60', 'gray61', 'gray62', 'gray63', 'gray64', 'gray65',
+        'gray66', 'gray67', 'gray68', 'gray69', 'gray70', 'gray71', 'gray72', 'gray73', 'gray74',
+        'gray75', 'gray76', 'gray77', 'gray78', 'gray79', 'gray80', 'gray81', 'gray82', 'gray83',
+        'gray84', 'gray85', 'gray86', 'gray87', 'gray88', 'gray89', 'gray90', 'gray91', 'gray92',
+        'gray93', 'gray94', 'gray95', 'gray97', 'gray98', 'gray99']
+        
+        self.main_rect = self.canvas.create_rectangle(self.x, CANVAS_HEIGHT, self.x + self.width, CANVAS_HEIGHT-self.height, fill=random.choice(COLORS))
+        
     def cleanup(self):
         self.canvas.delete(self.main_rect)
 
@@ -141,7 +228,7 @@ class Plane():
         self.wing1_points = [40,28, 76,28, 94,48, 80,48]
         self.wing2_points = [52,16, 78,8, 94,8, 81,16]
         self.tail_points = [90,16, 110,0, 124,0, 116,16]
-        self.width = 124 # plane width
+        self.width = 10 # plane width
         self.draw()
 
     ''' reset the plane to its starting position at the start of a new level '''
@@ -168,6 +255,18 @@ class Plane():
     ''' move the plane however much it moves during one frame '''
     def move(self):
         self.position.move(-4 * speed, 0)
+        if self.position.getX() < -self.width:
+            self.position.move(CANVAS_WIDTH, 40)
+            #ensure we don't go off the bottom of the screen
+            if self.position.getY() > CANVAS_HEIGHT:
+                self.position.Y = CANVAS_HEIGHT
+            #we get 10 points each row the plane moves down
+            return 10
+        else:
+            return 0
+
+    def movetoend(self):
+        self.position.move(-25 * speed, 0)
         if self.position.getX() < -self.width:
             self.position.move(CANVAS_WIDTH, 40)
             #ensure we don't go off the bottom of the screen
@@ -223,7 +322,7 @@ class Display(Frame):
             building.cleanup()
 
         #create the new ones
-        for building_num in range(0, 1200//SPACING):
+        for building_num in range(0, 1200//SPACING ):
             height = self.rand.randint(10,500) #random number between 10 and 500
             self.buildings.append(Building(self.canvas, building_num, height,
                                            self.building_width))
@@ -233,8 +332,13 @@ class Display(Frame):
 
     ''' check the state of the bomb each frame '''
     def check_bomb(self):
+
         if not self.bomb.falling:
             return
+
+        if self.bomb.position.getY() >= CANVAS_HEIGHT - 5:
+            self.bomb.explode()
+
         # did the bomb hit a building?
         for building in self.buildings:
             if building.is_inside(self.bomb.position):
@@ -245,6 +349,8 @@ class Display(Frame):
     def check_plane(self):
         # we'll check if the plane nose hits a building, or if the
         # base of the fuselage hits, or if the wing hits
+        
+        numofdestroyedbuildings = 0
         plane_nose = self.plane.position.copy()
         plane_nose.move(0, 28)
         plane_body_bottom = self.plane.position.copy()
@@ -256,7 +362,15 @@ class Display(Frame):
                 or building.is_inside(plane_body_bottom)
                 or building.is_inside(plane_wing)):
                 self.game_over()
-        if plane_body_bottom.getY() == CANVAS_HEIGHT and plane_body_bottom.getX() < 20:
+            if building.height <= 0:
+                numofdestroyedbuildings += 1
+                
+
+        if numofdestroyedbuildings == len(self.buildings):
+            print("All buildings have been destroyed")
+            self.plane.movetoend()
+        if plane_body_bottom.getY() == 672 and plane_body_bottom.getX() <= 40:
+            print("Plane has landed")
             self.plane_landed()
 
     ''' game_over is called when the plane crashes to stop play and display the 
@@ -264,6 +378,7 @@ class Display(Frame):
     def game_over(self):
         self.game_running = False
         self.won = False
+
         self.text = self.canvas.create_text(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, anchor="c")
         self.canvas.itemconfig(self.text, text="GAME OVER!", font=self.bigfont)
 
@@ -365,5 +480,5 @@ class Game():
             self.checkspeed()
         self.root.destroy()
 
-game = Game();
+game = Game()
 game.run()
